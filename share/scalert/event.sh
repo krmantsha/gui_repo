2f459c7c706c1296981c06d83f3260a64e2a97c9
Dirk Roessler <roessler@gempa.de>
2021-07-20 14:32:54 +0000

    Add changes by CGS

--- share/scalert/event.sh
+++ share/scalert/event.sh
@@ -5,7 +5,15 @@ echo " $1"
 if [ "$2" = "1" ]; then
 	aplay ~/.seiscomp/siren03.wav
 	echo " $1" | sed 's/,/, ,/g'   | festival --tts;
+	/home/rtt/seiscomp/share/scalert/gm_hl.sh $3 > /home/rtt/seiscomp/share/scalert/emailInfo.txt
+      /home/rtt/seiscomp/bin/seiscomp exec scbulletin -d mysql://sysop:sysop@localhost/seiscomp -E $3 >> /home/rtt/seiscomp/share/scalert/emailInfo.txt 
+	cat /home/rtt/seiscomp/share/scalert/emailInfo.txt |mail -s "$1,arrivals $4, eventID:$3" seis.networks@gmail.com;
+	mysql -u root -pzxcvbnm  seiscomp < /home/rt/seiscomp/share/scalert/update_webpage_events.sql
 else
-	echo " $1" | sed 's/,/, ,/g'   | festival --tts;
+	/home/rtt/seiscomp/share/scalert/gm_hl.sh $3 > /home/rtt/seiscomp/share/scalert/emailInfo.txt
+	/home/rtt/seiscomp/bin/seiscomp exec scbulletin -d mysql://sysop:sysop@localhost/seiscomp -E $3 >> /home/rtt/seiscomp/share/scalert/emailInfo.txt
+	cat /home/rtt/seiscomp/share/scalert/emailInfo.txt |mail -s "Update - $1,arrivals $4, eventID:$3" seis.networks@gmail.com;
+	mysql -u root -pzxcvbnm  seiscomp < /home/rtt/seiscomp/share/scalert/update_webpage_events.sql
+
 fi
 
