91cc8a02cdfcc59443b5c3687cfa79c45692c691
Dirk Roessler <roessler@gempa.de>
2023-01-25 07:18:28 +0000

    [configuration] Commit all previous changes made by CGS

--- share/scalert/email_alert.sh
+++ share/scalert/email_alert.sh
@@ -3,29 +3,29 @@
 echo " $1" 
 
 if [ "$2" = "1" ]; then
-#	aplay ~/.seiscomp3/siren03.wav
+#	aplay ~/.seiscomp/siren03.wav
 #	echo " $1" | sed 's/,/, ,/g'   | festival --tts;
-	/home/rt/seiscomp3/share/scalert/gm_hl.sh $3 > /home/rt/seiscomp3/share/scalert/emailInfo.txt
-        /home/rt/seiscomp3/bin/seiscomp exec scbulletin -d mysql://sysop:sysop@localhost/seiscomp3 -E $3 >> /home/rt/seiscomp3/share/scalert/emailInfo.txt 
-	cat /home/rt/seiscomp3/share/scalert/emailInfo.txt |mail -s "$1,arrivals $4, eventID:$3" seis_networks@geoscience.org.za;
+	/home/rtt/seiscomp/share/scalert/gm_hl.sh $3 > /home/rtt/seiscomp/share/scalert/emailInfo.txt
+        /home/rtt/seiscomp/bin/seiscomp exec scbulletin -d mysql://sysop:sysop@localhost/seiscomp -E $3 >> /home/rtt/seiscomp/share/scalert/emailInfo.txt 
+	cat /home/rtt/seiscomp/share/scalert/emailInfo.txt |mail -s "$1,arrivals $4, eventID:$3" stt@geoscience.org.za;
 	mag=` grep "M=" emailInfo.txt |awk -F= '{print $2}'  |awk '{print $1}'`
 	MAG=$(echo "scale=2;$mag*1"|bc)
 	if [ $MAG >= 3 ]
 	then
-		cat /home/rt/seiscomp3/share/scalert/emailInfo.txt |mail -s "$1,arrivals $4, eventID:$3" rmantsha@geoscience.org.za;
+		cat /home/rtt/seiscomp/share/scalert/emailInfo.txt |mail -s "$1,arrivals $4, eventID:$3" stt@geoscience.org.za;
 	fi
 	# temp=$(echo "scale=2;$tempvolt*1"|bc)	
 	#mysql -u root -pzxcvbnm  seiscomp3 < /home/rt/seiscomp3/share/scalert/update_webpage_events.sql
 else
-	/home/rt/seiscomp3/share/scalert/gm_hl.sh $3 > /home/rt/seiscomp3/share/scalert/emailInfo.txt
-	/home/rt/seiscomp3/bin/seiscomp exec scbulletin -d mysql://sysop:sysop@localhost/seiscomp3 -E $3 >> /home/rt/seiscomp3/share/scalert/emailInfo.txt
-	cat /home/rt/seiscomp3/share/scalert/emailInfo.txt |mail -s "Update - $1,arrivals $4, eventID:$3" seis_networks@geoscience.org.za;
+	/home/rtt/seiscomp/share/scalert/gm_hl.sh $3 > /home/rtt/seiscomp/share/scalert/emailInfo.txt
+	/home/rtt/seiscomp/bin/seiscomp exec scbulletin -d mysql://sysop:sysop@localhost/seiscomp -E $3 >> /home/rtt/seiscomp/share/scalert/emailInfo.txt
+	cat /home/rtt/seiscomp/share/scalert/emailInfo.txt |mail -s "Update - $1,arrivals $4, eventID:$3" stt@geoscience.org.za;
 	#mysql -u root -pzxcvbnm  seiscomp3 < /home/seiscomp/seiscomp3/share/scalert/update_webpage_events.sql
 	mag=` grep "M=" emailInfo.txt |awk -F= '{print $2}'  |awk '{print $1}'`
         MAG=$(echo "scale=2;$mag*1"|bc)
         if [ $MAG >= 3 ]
         then
-                cat /home/rt/seiscomp3/share/scalert/emailInfo.txt |mail -s "Update - $1,arrivals $4, eventID:$3" rmantsha@geoscience.org.za;
+                cat /home/rtt/seiscomp/share/scalert/emailInfo.txt |mail -s "Update - $1,arrivals $4, eventID:$3" rmantsha@geoscience.org.za;
         fi
 
 fi
--- share/scalert/event.sh
+++ share/scalert/event.sh
@@ -1,11 +1,20 @@
-#!/bin/sh
+ #!/bin/sh
 
 echo " $1" 
 
 if [ "$2" = "1" ]; then
-	aplay ~/.seiscomp/siren03.wav
-	echo " $1" | sed 's/,/, ,/g'   | festival --tts;
+#	aplay ~/.seiscomp3/siren03.wav
+#	echo " $1" | sed 's/,/, ,/g'   | festival --tts;
+#	/home/seiscomp/seiscomp3/share/scalert/gm_hl.sh $3 > /home/seiscomp/seiscomp3/share/scalert/emailInfo.txt
+ #      /home/seiscomp/seiscomp3/bin/seiscomp exec scbulletin -d mysql://sysop:sysop@localhost/seiscomp3 -E $3 >> /home/seiscomp/seiscomp3/share/scalert/emailInfo.txt 
+#	cat /home/seiscomp/seiscomp3/share/scalert/emailInfo.txt |mail -s "$1,arrivals $4, eventID:$3" seis.networks@gmail.com;
+	mysql -u root -pzxcvbnm  seiscomp < /home/rtt/seiscomp/share/scalert/update_webpage_events.sql
 else
-	echo " $1" | sed 's/,/, ,/g'   | festival --tts;
+#	/home/seiscomp/seiscomp3/share/scalert/gm_hl.sh $3 > /home/seiscomp/seiscomp3/share/scalert/emailInfo.txt
+#	/home/seiscomp/seiscomp3/bin/seiscomp exec scbulletin -d mysql://sysop:sysop@localhost/seiscomp3 -E $3 >> /home/seiscomp/seiscomp3/share/scalert/emailInfo.txt
+#	cat /home/seiscomp/seiscomp3/share/scalert/emailInfo.txt |mail -s "Update - $1,arrivals $4, eventID:$3" seis.networks@gmail.com;
+	mysql -u root -pzxcvbnm  seiscomp < /home/rtt/seiscomp/share/scalert/update_webpage_events.sql
+
 fi
+
 
