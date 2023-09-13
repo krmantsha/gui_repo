c6121bfb688d28ed3168c6963db012ce2a9cd975
Dirk Roessler <roessler@gempa.de>
2023-09-13 09:39:46 +0000

    [installation] Update SC to v. 5.5.5

--- share/scalert/gm_hl.sh
+++ share/scalert/gm_hl.sh
@@ -1,3 +1,3 @@
-lat=`scxmldump -fap -E $1  -d mysql://sysop:sysop@localhost/seiscomp3  |grep -n1 "<latitude>" |grep value |awk -F'[<>]' '{print $3}' `
-lon=`scxmldump -fap -E $1  -d mysql://sysop:sysop@localhost/seiscomp3  |grep -n1 "<longitude>" |grep value |awk -F'[<>]' '{print $3}'`
+lat=`scxmldump -fap -E $1  -d mysql://sysop:sysop@localhost/seiscomp  |grep -n1 "<latitude>" |grep value |awk -F'[<>]' '{print $3}' `
+lon=`scxmldump -fap -E $1  -d mysql://sysop:sysop@localhost/seiscomp  |grep -n1 "<longitude>" |grep value |awk -F'[<>]' '{print $3}'`
 echo "http://maps.google.com/?q=$lat,$lon&t=h&ll=$lat,$lon&z=12"
