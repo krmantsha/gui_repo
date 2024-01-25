ea49dd9c4bf0936d0bed9a435d860f1bf784e5b2
Dirk Roessler <roessler@gempa.de>
2024-01-25 15:07:12 +0000

    [installation] Upgrade SC to version 5.5.17

--- share/templates/seedlink/mseedfifo/setup.py
+++ share/templates/seedlink/mseedfifo/setup.py
@@ -2,19 +2,28 @@
 Plugin handler for the mseedfifo plugin.
 '''
 
+try:
+    import seiscomp.system
+    hasSystem = True
+except:
+    hasSystem = False
+
+
 class SeedlinkPluginHandler:
   # Create defaults
   def __init__(self):
     pass
 
   def push(self, seedlink):
-    pass
-
-  def flush(self, seedlink):
     # Check and set defaults
     address = "%s/%s" % (seedlink.run_dir,'mseedfifo')
-    try: address = seedlink.param('plugins.mseedfifo.fifo', False)
-    except: address = "%s/%s" % (seedlink.run_dir,'mseedfifo')
+    try:
+        address = seedlink.param('plugins.mseedfifo.fifo', False)
+        if hasSystem:
+            e = seiscomp.system.Environment.Instance()
+            address = e.absolutePath(address)
+    except:
+        address = "%s/%s" % (seedlink.run_dir,'mseedfifo')
 
     seedlink.setParam('plugins.mseedfifo.fifo_param', address, False)
 
@@ -27,3 +36,6 @@ class SeedlinkPluginHandler:
         noexit = ''
     except: noexit = ''
     seedlink.setParam('plugins.mseedfifo.noexit_param', noexit, False)
+
+  def flush(self, seedlink):
+    self.push(seedlink)
